af621f74b1f2bb86ce3e472767120e8e18509bb9
mahfuz380 <almahfuz380@gmail.com>
2020-04-18 14:00:46 +0600

    class inheritance done

---
+++ class_inheritance.php
@@ -0,0 +1,22 @@
+<?php
+class Cars {
+	var $wheels =4;
+
+	
+	function car_detail(){ //if function declared inside class it is called methods
+return  "hello";
+
+	}
+
+}
+$bmw = new Cars();
+
+class Trucks extends Cars{ //class trucks inherits class Cars
+//var $wheels =10;
+}
+
+$tacoma = new Trucks();
+echo $tacoma->wheels;
+
+
+?>
